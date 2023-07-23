214cc7823e4e04e418fb844a3d7f65b2c81d71df
KT15234 <139129152+KT15234@users.noreply.github.com>
2023-07-23 22:59:36 +0530

    Add files via upload

---
+++ app/updateintern/updateintern.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-updateintern',
+  templateUrl: './updateintern.component.html',
+  styleUrls: ['./updateintern.component.css']
+})
+export class UpdateinternComponent {
+
+}
